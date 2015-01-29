4a0087d6fca66311f6ca05433f1160308e4d2e7f
Farida Jhabvala Romero <faridaj@corn30.stanford.edu>
2015-01-29 13:46:33 -0800

    death row texas

---
+++ homework/death-row-parsing/helper.sh
@@ -0,0 +1,3 @@
+mkdir -p data-hold
+curl http://www.tdcj.state.tx.us/death_row/dr_offenders_on_dr.html -o data-hold/txdr.html
+curl http://www.tdcj.state.tx.us/death_row/dr_list_all_dr_1923-1973.html -o data-hold/txdr2.html
