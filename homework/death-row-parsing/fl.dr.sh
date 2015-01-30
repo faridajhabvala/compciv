9bec1cfcb5846791d891d0e81c89c8633de51a68
Farida Jhabvala Romero <faridaj@corn03.stanford.edu>
2015-01-30 11:42:50 -0800

    got tx and fl

---
+++ homework/death-row-parsing/fl.dr.sh
@@ -0,0 +1,2 @@
+cat data-hold/fldr.html data-hold/fldr2.html data-hold/fldr3.html | pup 'td:nth-of-type(3) text{}'
+
--- homework/death-row-parsing/helper.sh
+++ homework/death-row-parsing/helper.sh
@@ -1,3 +1,8 @@
 mkdir -p data-hold
 curl http://www.tdcj.state.tx.us/death_row/dr_offenders_on_dr.html -o data-hold/txdr.html
-curl http://www.tdcj.state.tx.us/death_row/dr_list_all_dr_1923-1973.html -o data-hold/txdr2.html
+curl http://www.tdcj.state.tx.us/death_row/dr_list_all_dr_1923-1973.html -o data-hold/txdr2.html 
+curl http://www.dc.state.fl.us/activeinmates/deathrowroster.asp -o data-hold/fldr.html 
+curl http://www.dc.state.fl.us/oth/deathrow/execlist.html -o data-hold/fldr2.html 
+curl http://www.dc.state.fl.us/oth/deathrow/execlist2.html -o data-hold/fldr3.html 
+curl http://www.cdcr.ca.gov/capital_punishment/docs/condemnedinmatelistsecure.pdf -o data-hold/cadr.pdf 
+
