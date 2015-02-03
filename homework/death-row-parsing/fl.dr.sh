5d3dfddfbc359914836187377476f588c8659c65
Farida Jhabvala Romero <faridaj@corn04.stanford.edu>
2015-02-02 16:14:32 -0800

    changes to FL

--- homework/death-row-parsing/fl.dr.sh
+++ homework/death-row-parsing/fl.dr.sh
@@ -1,2 +1,3 @@
-cat data-hold/fldr.html data-hold/fldr2.html data-hold/fldr3.html | pup 'td:nth-of-type(3) text{}'
+cat data-hold/fldr.html data-hold/fldr2.html data-hold/fldr3.html | pup 'td:nth-of-type(3) text{}' | grep -oE '\b[[:upper:]]{2}\b'
+
 
