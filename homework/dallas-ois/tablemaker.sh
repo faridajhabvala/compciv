37c5362fdce83905bc56a506cd0c81ea82dc983d
Farida Jhabvala Romero <faridaj@corn30.stanford.edu>
2015-03-19 19:41:21 -0700

    bit more

--- homework/dallas-ois/tablemaker.sh
+++ homework/dallas-ois/tablemaker.sh
@@ -1,4 +1,4 @@
-mkdir - p ./table 
+mkdir tables 
 cat ./data-hold/*.csv | grep -v 'Location' | csvfix echo -osep '|' | while read -r incident; do
 #    csvfix order -smq -f 4 | grep -v 'Location' | tr -d '"' | 
     locationslug=$(echo $incident | csvfix order -sep '|' -smq -f 4 | tr ' ' '+') 
@@ -18,8 +18,14 @@ echo "case_number|date|location|suspect_status|suspect_weapon|suspects|officers|
  echo "$incident_sanspdf|$latitude|$longitude|$narrative" 
 done > tables/incidents.psv
 
+echo "case_number, date, suspect_killed, suspect_weapon, last_name, first_name, race, gender"
 cat ./tables/incidents.psv | while read -r rows; do
 officernames=$(echo $rows | cut -d '|' -f 7 | grep -v 'officers' | sed "s/\/M/\/M#/g" | sed "s/\/F/\/M#/g" | grep -oE "[^#]+")
-echo $officernames 
+status=$(echo $rows | cut -d '|' -f 4 | grep -v 'suspect_status') 
+casen=$(echo $rows | cut -d '|' -f 1 | grep -v 'case_number')
+date=$(echo $rows | cut -d '|' -f 2 | grep -v 'date')
+weapon=$(echo $rows | cut -d '|' -f 5 | grep -v 'suspect_weapon')
+echo $casen $date $status $weapon $officernames 
 done > tables/officers.psv
+
 
